98ce92bf298bee944cf38902d76753b96b6a9033
Daniele Lima <daniele-_darlane@hotmail.com>
2022-01-06 16:39:58 -0300

    colocou comentário sobre o valor para conversão

--- exercicio.py
+++ exercicio.py
@@ -1,6 +1,6 @@
 # Conversor de moeda
 print("\nConvertendo um valor do dólar para o real.")
 dol = float(input("Forneça um valor em dólar: "))
-conv = 5.69
+conv = 5.69  # Cotação do dólar no momento
 real = dol * conv
 print(f"\nU$D {dol} equivale a R$ {real}.\n")
