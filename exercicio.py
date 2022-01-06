52ad5c0cccf6f95f4d88d8a7f85c805ed7e612ab
Daniele Lima <daniele-_darlane@hotmail.com>
2022-01-06 16:37:46 -0300

    criado um conversor de dólar para real

---
+++ exercicio.py
@@ -0,0 +1,6 @@
+# Conversor de moeda
+print("\nConvertendo um valor do dólar para o real.")
+dol = float(input("Forneça um valor em dólar: "))
+conv = 5.69
+real = dol * conv
+print(f"\nU$D {dol} equivale a R$ {real}.\n")
