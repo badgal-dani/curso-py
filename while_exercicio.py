683818937324943d94adf037e665b909da4f881a
Daniele Lima <daniele-_darlane@hotmail.com>
2022-01-11 16:53:01 -0300

    concluiu

--- while_exercicio.py
+++ while_exercicio.py
@@ -1,7 +1,7 @@
-""" 1 - Faça um programa que leia um nome de usuário e a sua senha 
-e não aceite a senha igual ao nome do usuário, mostrando uma mensagem 
-de erro e voltando a pedir as informações.
-"""
+# 1 - Faça um programa que leia um nome de usuário e a sua senha 
+# e não aceite a senha igual ao nome do usuário, mostrando uma mensagem 
+# de erro e voltando a pedir as informações.
+
 user = input("Forneça um nome de usuário: ")
 while True:
     senha = input("Forneça uma senha diferente do nome de usuário fornecido: ")
@@ -11,10 +11,10 @@ while True:
     else:
         print("Senha não pode ser igual ao nome de usuário fornecido! Tente novamente.")
 
-""" 2 - Faça um programa que peça uma nota, entre zero e dez.
-Mostre uma mensagem caso o valor seja inválido e continue pedindo 
-até que o usuário informe um valor válido.
-"""
+# 2 - Faça um programa que peça uma nota, entre zero e dez.
+# Mostre uma mensagem caso o valor seja inválido e continue pedindo 
+# até que o usuário informe um valor válido.
+
 while True:
     nota = float(input("Forneça uma nota entre 0 e 10: "))
     if 0 <= nota <= 10:
@@ -23,7 +23,7 @@ while True:
     else:
         print("Valor incorreto.")
 
-# 2 - Faça um programa que imprima na tela apenas os números ímpares entre 1 e 50.
+# 3 - Faça um programa que imprima na tela apenas os números ímpares entre 1 e 50.
 n = 1
 while n <= 50:
     if n % 2 != 0:
