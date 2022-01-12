a169b7329d828eaaf79a90d2cb558ddabd137458
Daniele Lima <daniele-_darlane@hotmail.com>
2022-01-11 23:03:10 -0300

    questão 7

---
+++ lista_3/lista3_ex7.py
@@ -0,0 +1,5 @@
+# 7. Faça um programa que imprima na tela apenas os números ímpares entre 
+# 1 e 50.(Use range de intervalos (range(x, y))
+for i in range(51):
+    if i % 2 != 0:
+        print(i)
