b498aadbf958e2a52f35e90c13a5915e5533417f
Daniele Lima <daniele-_darlane@hotmail.com>
2022-01-11 23:01:22 -0300

    renomeado

--- lista_3/lista3_x6.py
+++
@@ -1,8 +0,0 @@
-# 6. Faça um programa que leia 5 números e informe o maior número. (Dica: 
-# Use lista, função .sort() e indexação negativa (pegar i item [-1]))
-nums = []
-for i in range(5):
-    n = float(input(f"Forneça o {i + 1}° número: "))
-    nums.append(n)
-
-print(f"O maior número é: {max(nums)}")
